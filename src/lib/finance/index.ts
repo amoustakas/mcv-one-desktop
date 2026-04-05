923a506bacc3f6d15b7b0f9fd429ecfb173cdc64
Tony Moustakas <tony@fullgain.ca>
2026-04-05 05:14:57 -0400

    feat(finance): add barrel export index

---
+++ src/lib/finance/index.ts
@@ -0,0 +1,8 @@
+// src/lib/finance/index.ts
+// Barrel export for the Finance Engine
+
+export * from './types';
+export * from './statements';
+export * from './metrics';
+export * from './cost-intelligence';
+export * from './revenue-recognition';
